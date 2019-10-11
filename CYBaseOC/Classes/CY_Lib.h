780e5d947d7a23ed1d3ba29c6289361b94f007de
zhangchunyong <2248895786@qq.com>
2019-10-11 10:55:54 +0800

    Test

--- CYBaseOC/Classes/CY_Lib.h
+++ CYBaseOC/Classes/CY_Lib.h
@@ -13,24 +13,24 @@
 #import <Foundation/Foundation.h>
 #import <UIKit/UIKit.h>
 
-// Masonry所需
-// 定义这个常量，就可以不用在开发过程中使用"mas_"前缀。
-#define MAS_SHORTHAND
-// 定义这个常量，就可以让Masonry帮我们自动把基础数据类型的数据，自动装箱为对象类型。
-#define MAS_SHORTHAND_GLOBALS
-#import "Masonry.h"
-
-#import "MJRefresh.h"
-
-#import "CY_BaseMVC.h"
-#import "CY_Category.h"
-#import "CY_Common.h"
-#import "CY_Extension.h"
-#import "CY_Application.h"
-#import "CY_Logger.h"
-#import "CY_Manager.h"
-#import "CY_third.h"
-#import "CY_Model.h"
+//// Masonry所需
+//// 定义这个常量，就可以不用在开发过程中使用"mas_"前缀。
+//#define MAS_SHORTHAND
+//// 定义这个常量，就可以让Masonry帮我们自动把基础数据类型的数据，自动装箱为对象类型。
+//#define MAS_SHORTHAND_GLOBALS
+//#import "Masonry.h"
+//
+//#import "MJRefresh.h"
+//
+//#import "CY_BaseMVC.h"
+//#import "CY_Category.h"
+//#import "CY_Common.h"
+//#import "CY_Extension.h"
+//#import "CY_Application.h"
+//#import "CY_Logger.h"
+//#import "CY_Manager.h"
+//#import "CY_third.h"
+//#import "CY_Model.h"
 
 
 #endif /* CY_Lib_h */
