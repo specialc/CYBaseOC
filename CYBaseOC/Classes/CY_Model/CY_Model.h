ae4f2dbb5f7a1424271b1ac402a533e5fa643779
zhangchunyong <2248895786@qq.com>
2019-10-11 12:03:33 +0800

    1.修改.podspec版本号0.1.3 2.添加CY_Model 3.添加JSONModel依赖

---
+++ CYBaseOC/Classes/CY_Model/CY_Model.h
@@ -0,0 +1,17 @@
+//
+//  CY_Model.h
+//  CYBase
+//
+//  Created by 张春咏 on 2019/7/14.
+//  Copyright © 2019 CY. All rights reserved.
+//
+/**
+ *  返回值反射对象
+ */
+
+#import "JSONModel.h"
+
+@interface CY_Model : JSONModel
+@property (nonatomic, assign) BOOL isCache;
++ (id)tempData;
+@end
---
+++ CYBaseOC/Classes/CY_Model/CY_Model.m
@@ -0,0 +1,21 @@
+//
+//  CY_Model.m
+//  CYBase
+//
+//  Created by 张春咏 on 2019/7/14.
+//  Copyright © 2019 CY. All rights reserved.
+//
+
+#import "CY_Model.h"
+
+@implementation CY_Model
+
++ (BOOL)propertyIsOptional:(NSString *)propertyName {
+    return YES;
+}
+
++ (id)tempData {
+    return nil;
+}
+
+@end
